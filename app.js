025c371171806ecafebbb46101bab69c0eaebf44
McGriff <hmcgriff@una.edu>
2022-09-01 13:23:24 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('node:path')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+ /// res.send('Hello World from Node/Express/Heroku')
+res.sendFile(path.join(__dirname, 'index.html'));
+
 })
+
 
 app.listen(process.env.PORT || 3000,
     () => console.log("Server is running..."));
